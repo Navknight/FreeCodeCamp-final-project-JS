706c7097bb518ae72ad5ca23701b842df0be0e1f
Abhinav <abhi.gupta8802@gmail.com>
2022-11-06 16:01:46 +0530

    ceaser cipher

---
+++ Ceaser-cipher.js
@@ -0,0 +1,17 @@
+function rot13(str) {
+    const regex = new RegExp(/[A-Z]/)
+    return str.split("").map((a,i) => {
+      if(regex.test(a)){
+        if(a.charCodeAt(0) + 13 <= 90){
+          return String.fromCharCode(a.charCodeAt(0) + 13);
+        }else{
+          return String.fromCharCode(a.charCodeAt(0) + 13 - 26);
+        }
+      }
+      else{
+        return a;
+      }
+    }).join("");
+  }
+  
+  console.log(rot13("SERR PBQR PNZC"));
