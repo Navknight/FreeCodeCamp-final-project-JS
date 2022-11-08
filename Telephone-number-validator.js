67ebe25555490e93c69b5e3bb45f1923ff0efa88
Abhinav <abhi.gupta8802@gmail.com>
2022-11-08 12:26:10 +0530

    telephone

--- Telephone-number-validator.js
+++ Telephone-number-validator.js
@@ -1,8 +1,9 @@
 function telephoneCheck(str) {
-  let regex = [
-
+  let regex = [/^(1\s*)?\(\d{3}\)(-|\s)*\d{3}(-|\s)*\d{4}$/,
+    /^\d{10}$/,
+    /^(1\s*)?\d{3}(-|\s)\d{3}(-|\s)\d{4}$/,
   ]
-  return true;
+  return regex.some((a) => a.test(str));
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("1 555-555-5555"));
---
+++ test.js
@@ -0,0 +1,1 @@
+//what will the regex /^(1 )\(\d{3}\)(-|\s)\d{3}(-|\s)\d{4}$/ match?
